f4c653faa424228c3993e5b6f3e42ad001beca6a
Mindgamesnl <matsmoolhuizen@gmail.com>
2017-04-04 08:41:09 +0200

    Create NL.js

---
+++ NL.js
@@ -0,0 +1,27 @@
+langpack = {};
+langpack.hue = {};
+langpack.message = {};
+langpack.notification = {};
+
+
+
+langpack.message.welcome = "Verbonden als %name%! Welkom! :)";
+langpack.message.notconnected = "Je bent niet in de server.";
+langpack.message.server_is_offline = "Server is offline.";
+langpack.message.inavlid_url = "Ongeldige url.";
+langpack.message.invalid_connection = "Ongeldige verbinding!";
+langpack.message.reconnect_prompt = "Deze url in ongeldig, verbind opnieuw via <b>/audio</b> of <b>/connect</b>.";
+langpack.message.socket_closed = "Verbinding is verbroken, een moment geduld...";
+langpack.message.mobile_qr = "Qr code voor mobiele versie.";
+langpack.message.listen_on_soundcloud = "luister op soundcloud!";
+
+langpack.notification.header = "OpenAudioMc | %username%";
+langpack.notification.img = 'files/images/footer_logo.png';
+
+langpack.hue.disabled = "Philips hue lampen zijn uitgeschakeld door de server admin!";
+langpack.hue.please_link = "<h2>philips hue lampen gevonden!</h2><h3>druk op de hue-link knop!</h3>";
+langpack.hue.connecting = "<h2>Verbinden met hue-bridge...</h2>";
+langpack.hue.re_search_bridge = "<h2>Geen philips hue bridge gevonden :(</h2><h4>Aan het zoeken naar een nieuwe bridge in je netwerk...</h4>";
+langpack.hue.cant_connect = "<h2>Kon niet verbinden met je hue-bridge :(</h2>";
+langpack.hue.not_found = "<h2>Geen hue bridge gevonden :(</h2>";
+langpack.hue.connected_with_bridge = "<h3>Je bent nu verbonden met je %bridgename% bridge.<br />Veel plezier! :)</h3>";
