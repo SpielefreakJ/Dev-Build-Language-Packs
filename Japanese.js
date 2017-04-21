3edc591133a0ec3cf6ccfb088b13147b0a81b425
Hogenami <dev@tensi.org>
2017-04-21 18:42:32 +0900

    翻訳
    ほげえええwww

---
+++ Japanese.js
@@ -0,0 +1,27 @@
+langpack = {};
+langpack.hue = {};
+langpack.message = {};
+langpack.notification = {};
+
+
+
+langpack.message.welcome = "%name% として接続しました,ようこそ!";
+langpack.message.notconnected = "サーバに接続されていません。";
+langpack.message.server_is_offline = "サーバはオフラインです(堕ちています)。";
+langpack.message.inavlid_url = "URLが無効です。";
+langpack.message.invalid_connection = "接続が無効です。";
+langpack.message.reconnect_prompt = "残念ながら、このURLは無効なようです。 <b>/audio</b> もしくは <b>/connect</b>にて再発行をお願いします。";
+langpack.message.socket_closed = "サーバから切断されました。暫くお待ち下さい。";
+langpack.message.mobile_qr = "QRコード (モバイル端末対応)";
+langpack.message.listen_on_soundcloud = "Soundcloudから視聴中!";
+
+langpack.notification.header = "OpenAudioMc | %username%";
+langpack.notification.img = 'files/images/footer_logo.png';
+
+langpack.hue.disabled = "philips hue はサーバ管理者により無効化されています。";
+langpack.hue.please_link = "<h2>philips hue が検出されました。</h2><h3>リンクをクリックしてください!</h3>";
+langpack.hue.connecting = "<h2>hue bridgeに接続中…</h2>";
+langpack.hue.re_search_bridge = "<h2>hue bridge が見つかりません。</h2><h4>あなたのネットワークから検索中です…</h4>";
+langpack.hue.cant_connect = "<h2>hue bridge に接続できません。</h2>";
+langpack.hue.not_found = "<h2>ネットワーク内にhue bridge が存在しません。</h2>";
+langpack.hue.connected_with_bridge = "<h3>%bridgename% bridge に接続しました。<br />楽しんで! </h3>";
