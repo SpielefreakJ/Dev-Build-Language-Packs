f87f142b6b9f9aa81560989e4bf88bee20bdced2
Legoman99573 <christianwhead@gmail.com>
2017-09-04 14:04:29 -0500

    Added more messages

--- English.js
+++ English.js
@@ -4,7 +4,7 @@ langpack.message = {};
 langpack.notification = {};
 
 
-
+langpack.message.devbuild = "This server is using a Development Build! Some functions may or may not work."
 langpack.message.welcome = "Connected as %name%! Welcome!";
 langpack.message.notconnected = "You're not connected to the server...";
 langpack.message.server_is_offline = "Server is offline.";
@@ -14,14 +14,26 @@ langpack.message.reconnect_prompt = "Sorry but your url is not valid  Please req
 langpack.message.socket_closed = "Disconnected from the server, please wait";
 langpack.message.mobile_qr = "Qr code for mobile client";
 langpack.message.listen_on_soundcloud = "Listen on soundcloud!";
+langpack.message.clypit_url = "<h2>Uh oh</h2> <h3>Clyp.it is no longer supported! <b>Do not ask for support.</b></h3>"
+langpack.message.spotify_url = "<h2>Uh oh</h2> <h3>Spotify links do not currently work. <b>Support is coming soon™</b></h3>"
+langpack.message.minime_disabled = "<h2>Minimi is disabled by the server admin.</h2>"
+langpack.message.debugger_off = "<h2>Debugger is only available on development builds.</h2>"
+langpack.message.easter_egg = "<h2>You found an EasterEgg.</h2><h3>Now listen to the torture >:)</h3>"
 
 langpack.notification.header = "OpenAudioMc | %username%";
 langpack.notification.img = 'files/images/footer_logo.png';
 
 langpack.hue.disabled = "philips hue lights are disabled by the server admin!";
-langpack.hue.please_link = "<h2>philips hue lights detected!</h2><h3>Please press the link button!</h3>";
+langpack.hue.please_link = "<h2>Philips HUE bridge detected!</h2><h3>Please press the link button!</h3>";
 langpack.hue.connecting = "<h2>Connecting to hue bridge...</h2>";
 langpack.hue.re_search_bridge = "<h2>No philips hue bridge found</h2><h4>Searching for a hue bridge in your network...</h4>";
 langpack.hue.cant_connect = "<h2>Could not connect to hue bridge</h2>";
 langpack.hue.not_found = "<h2>No philips hue bridge found</h2>";
-langpack.hue.connected_with_bridge = "<h3>You are now connected with your %bridgename% bridge.<br />have fun!</h3>";
+langpack.hue.connected_with_bridge = "<h3>You are now connected with your %bridgename% bridge. Have fun!</h3>";
+langpack.hue.ssl_error = "<h2>There is no support over a secure connection :(</h2><h4>Phillips HUE support is automatically disabled.</h4>"
+langpack.hue.direct_ip_prompt = "<h2>Please input your bridge IP</h2>"
+langpack.hue.direct_ip_prompt_empty = "<h2>Please enter a Bridge IP</h2>"
+langpack.hue.direct_ip_lookup = "<h2>Attempting to connect to bridge IP %ip%...</h2>"
+langpack.hue.direct_ip_lookup_success = "<h2>Found bridge IP %ip%</h2><h3>Connecting...</h3>"
+langpack.hue.direct_ip_lookup_failed = "<h2>Failed to connect to bridge IP %ip%...</h2>"
+langpack.hue.light_data_fail = "<h2>Unable to retrieve Light Data.</h2><h3>Press <b>F5</b> to refresh</h3>"
