0694eb9e8e3361f46b9729130db4346e9050ffb1
Legoman99573 <christianwhead@gmail.com>
2017-11-17 17:58:09 -0600

    Better Pirate Translation

--- PirateSpeak.js
+++ PirateSpeak.js
@@ -4,24 +4,36 @@ langpack.message = {};
 langpack.notification = {};
 
 
-
-langpack.message.welcome = "yarr connected as %name%! Welcome!";
-langpack.message.notconnected = "yarr not connected to the server...";
-langpack.message.server_is_offline = "Ship is offline.";
-langpack.message.inavlid_url = "Invalid map.";
-langpack.message.invalid_connection = "Invalid boat!";
-langpack.message.reconnect_prompt = "Sorry but your boat is not valid  Please request a new ship via <b>/audio</b> or <b>/connect</b>.";
-langpack.message.socket_closed = "Lost from the ship, please wait";
-langpack.message.mobile_qr = "Qr code for mobile pirate-map";
-langpack.message.listen_on_soundcloud = "Listen on arrcloud!";
+langpack.message.devbuild = "This server be usin' a Development Build! Some functions may or may nah work.";
+langpack.message.welcome = "Connected as %name%! Ahoy!";
+langpack.message.notconnected = "Ye're nah connected t' th' server...";
+langpack.message.server_is_offline = "Server be offline.";
+langpack.message.inavlid_url = "Invalid url.";
+langpack.message.invalid_connection = "Invalid connection!";
+langpack.message.reconnect_prompt = "Sorry but yer url ain't valid  request a new url via <b>/audio</b> or <b>/connect</b>.";
+langpack.message.socket_closed = "Disconnected from th' server, wait";
+langpack.message.mobile_qr = "Qr code fer mobile client";
+langpack.message.listen_on_soundcloud = "Listen on soundcloud!";
+langpack.message.clypit_url = "<h2>Uh oh</h2> <h3>Clyp.it is no longer supported! <b>Do nah ask fer support.</b></h3>";
+langpack.message.spotify_url = "<h2>Uh oh</h2> <h3>Spotify links do nah currently work. <b>Support be comin' soon™</b></h3>";
+langpack.message.minime_disabled = "<h2>Minimi be disabled by th' server admin.</h2>";
+langpack.message.debugger_off = "<h2>Debugger be only available on development builds.</h2>";
+langpack.message.easter_egg = "<h2>Ye found an EasterEgg.</h2><h3>Now listen t' th' torture >:)</h3>";
 
 langpack.notification.header = "OpenAudioMc | %username%";
 langpack.notification.img = 'files/images/footer_logo.png';
 
-langpack.hue.disabled = "philips hue lights arr disabled by the boad captain!";
-langpack.hue.please_link = "<h2>philips hue lights found onmap!</h2><h3>please turn the link wheel!</h3>";
-langpack.hue.connecting = "<h2>Screaming to hue boat...</h2>";
-langpack.hue.re_search_bridge = "<h2>No philips hue boat found</h2><h4>Searching for a hue bridge in your map...</h4>";
-langpack.hue.cant_connect = "<h2>Could not connect to hue map</h2>";
-langpack.hue.not_found = "<h2>No philips hue map found</h2>";
-langpack.hue.connected_with_bridge = "<h3>You are teamed up with the %bridgename% boat.<br />Go ahead mate!</h3>";
+langpack.hue.disabled = "philips hue lights are disabled by th' server admin!";
+langpack.hue.please_link = "<h2>Philips HUE bridge detected!</h2><h3>press th' link button!</h3>";
+langpack.hue.connecting = "<h2>Connectin' t' hue bridge...</h2>";
+langpack.hue.re_search_bridge = "<h2>No philips hue bridge found</h2><h4>Searchin' fer a hue bridge in yer network...</h4>";
+langpack.hue.cant_connect = "<h2>Could nah connect t' hue bridge</h2>";
+langpack.hue.not_found = "<h2>No philips hue bridge found</h2>";
+langpack.hue.connected_with_bridge = "<h3>Ye be now connected wit' yer %bridgename% bridge. 'ave fun!</h3>";
+langpack.hue.ssl_error = "<h2>Thar be no support o'er a secure connection :(</h2><h4>Phillips HUE support be automatically disabled.</h4>";
+langpack.hue.direct_ip_prompt = "<h2>Input yer bridge IP</h2>";
+langpack.hue.direct_ip_prompt_empty = "<h2>Board a Bridge IP</h2>";
+langpack.hue.direct_ip_lookup = "<h2>Attemptin' t' connect t' bridge IP %ip%...</h2>";
+langpack.hue.direct_ip_lookup_success = "<h2>Found bridge IP %ip%</h2><h3>Connectin'...</h3>";
+langpack.hue.direct_ip_lookup_failed = "<h2>Failed t' connect t' bridge IP %ip%...</h2>";
+langpack.hue.light_data_fail = "<h2>Unable t' loot Light Data.</h2><h3>Press <b>F5</b> t' refresh</h3>";
